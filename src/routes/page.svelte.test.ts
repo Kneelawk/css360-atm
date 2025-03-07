a9ebf17d500635abda6cca55cfe4d3aa799bed92
Cyan Kneelawk <kneelawk@gmail.com>
2025-03-06 21:21:27 -0800

    Add home-page test

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -1,11 +1,11 @@
-// import { describe, test, expect } from 'vitest';
-// import '@testing-library/jest-dom/vitest';
-// import { render, screen } from '@testing-library/svelte';
-// import Page from './+page.svelte';
-//
-// describe('/+page.svelte', () => {
-// 	test('should render h1', () => {
-// 		render(Page);
-// 		expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
-// 	});
-// });
+import { describe, test, expect } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { render, screen } from '@testing-library/svelte';
+import Page from './+page.svelte';
+
+describe('/+page.svelte', () => {
+	test('should render h1', () => {
+		render(Page, { data: { user: { username: 'test-user', id: 'test-id' } } });
+		expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hi, test-user!');
+	});
+});
