0dbeb3e0e3b0579eb09982b1ace9385fae5414d9
Cyan Kneelawk <kneelawk@gmail.com>
2025-03-06 21:53:12 -0800

    Add account and logging tables

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import {pgTable, serial, text, integer, timestamp, real} from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
@@ -16,6 +16,55 @@ export const session = pgTable('session', {
 	userAgent: text('user_agent')
 });
 
-export type Session = typeof session.$inferSelect;
+export const account = pgTable('account', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	amount: real('amount')
+		.notNull()
+		.default(0)
+})
 
+export const deposit = pgTable('deposit', {
+	id: text('id').primaryKey(),
+	date: timestamp('date').notNull(),
+	accountId: text('account_id')
+		.notNull()
+		.references(() => account.id),
+	initialAccountAmount: real('initial_account_amount').notNull().default(0),
+	depositAmount: real('deposit_amount').notNull().default(0)
+})
+
+export const withdraw = pgTable('withdraw', {
+	id: text('id').primaryKey(),
+	date: timestamp('date').notNull(),
+	accountId: text('account_id')
+		.notNull()
+		.references(() => account.id),
+	initialAccountAmount: real('initial_account_amount').notNull().default(0),
+	withdrawAmount: real('withdraw_amount').notNull().default(0)
+})
+
+export const transfer = pgTable('transfer', {
+	id: text('id').primaryKey(),
+	date: timestamp('date').notNull(),
+	fromAccountId: text('from_account_id')
+		.notNull()
+		.references(() => account.id),
+	toAccountId: text('to_account_id')
+		.notNull()
+		.references(() => account.id),
+	transferAmount: real('transfer_amount').notNull().default(0),
+	fromInitialAccountAmount: real('from_initial_account_amount').notNull().default(0),
+	toInitialAccountAmount: real('to_initial_account_amount').notNull().default(0),
+})
+
+export type Session = typeof session.$inferSelect;
 export type User = typeof user.$inferSelect;
+
+export type Account = typeof account.$inferSelect;
+
+export type Deposit = typeof deposit.$inferSelect;
+export type Withdraw = typeof withdraw.$inferSelect;
+export type Transfer = typeof transfer.$inferSelect;
